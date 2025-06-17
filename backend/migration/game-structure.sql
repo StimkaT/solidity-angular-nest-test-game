3441b25584a09d2e5f52ec7de8393d1a2e09e2fe
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-06-17 11:48:53 +0300

    added bd

---
+++ backend/migration/game-structure.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: mysql:3306
+-- Время создания: Июн 17 2025 г., 08:34
+-- Версия сервера: 8.0.42
+-- Версия PHP: 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `games`
+--
+
+CREATE TABLE `games` (
+                         `id` int UNSIGNED NOT NULL,
+                         `contractAddress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `ownerAddress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `finished_at` timestamp NULL DEFAULT NULL,
+                         `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+                         `id` int UNSIGNED NOT NULL,
+                         `login` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `wallet` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `encrypted_private_key` text COLLATE utf8mb4_unicode_ci NOT NULL,
+                         `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `games`
+--
+ALTER TABLE `games`
+    ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+    ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `games`
+--
+ALTER TABLE `games`
+    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+    MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
