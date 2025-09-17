a731ca7cab762ee3eae8432dc021dd71a0e23214
alexeyrozhnov <Aliaksei.Razhnou@piarainc.com>
2025-09-18 02:24:59 +0300

    added auto reload game list

--- frontend/src/app/components/active-game-list/active-game-list.component.ts
+++ frontend/src/app/components/active-game-list/active-game-list.component.ts
@@ -1,13 +1,13 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {MatButton, MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { GameReloadService } from '../../services/game-reload.service';
 
 @Component({
   selector: 'app-active-game-list',
   standalone: true,
   imports: [
-    MatButton,
     MatButtonModule,
     MatDividerModule,
     MatIconModule
@@ -15,19 +15,27 @@ import {MatDividerModule} from '@angular/material/divider';
   templateUrl: './active-game-list.component.html',
   styleUrl: './active-game-list.component.scss'
 })
-export class ActiveGameListComponent {
+export class ActiveGameListComponent implements OnInit, OnDestroy {
   @Input() gameType: string = '';
   @Input() activeGamesList: any;
   @Input() player: any;
 
   @Output() emitter = new EventEmitter();
 
+  constructor(private reloadService: GameReloadService) {}
+
+  ngOnInit() {
+    this.reloadService.startReload((gameType) => {
+      this.event('reload', gameType);
+    }, this.gameType);
+  }
+
   event(event: string, title: string) {
     const message = {
       event: `ActiveGameListComponent:${event}`,
       title,
-    }
-    this.emitter.emit(message)
+    };
+    this.emitter.emit(message);
   }
 
   manage(manage: string, gameId: string, title: string) {
@@ -36,8 +44,11 @@ export class ActiveGameListComponent {
       wallet: this.player.wallet,
       gameId: gameId,
       title
-    }
-    this.emitter.emit(message)
+    };
+    this.emitter.emit(message);
   }
 
+  ngOnDestroy() {
+    this.reloadService.stopReload();
+  }
 }
